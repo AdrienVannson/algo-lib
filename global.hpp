d3cd866e2c4fb3872d38272f3dacec213b7ff806
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-12-30 23:05:48 +0100

    Ajout d'un fichier à inclure partout

---
+++ global.hpp
@@ -0,0 +1,20 @@
+#ifndef GLOBAL_HPP
+#define GLOBAL_HPP
+
+#define for_n(i,n) for (int i=0;i<n;i++)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+const int oo = 1000*1000*1000;
+
+inline int readInt ()
+{
+    int v;
+    cin >> v;
+    return v;
+}
+
+#endif // GLOBAL_HPP
