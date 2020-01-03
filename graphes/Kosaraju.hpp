810961aea14e8d16cebd000d9df83bb7e85315a3
Adrien VANNSON <adrien.vannson@protonmail.com>
2020-01-03 19:09:01 +0100

    Mise en forme

--- graphes/Kosaraju.hpp
+++ graphes/Kosaraju.hpp
@@ -10,55 +10,66 @@ class Kosaraju
 {
 public:
     Kosaraju (const Graphe &graphe) :
-        nbCFCs (0)
+        m_nbCFCs (0)
     {
-        CFCs.resize(graphe.nbNoeuds());
-        estVu.resize(graphe.nbNoeuds(), false);
+        m_CFCs.resize(graphe.nbNoeuds());
+        m_estVu.resize(graphe.nbNoeuds(), false);
 
         for (int i=0; i<graphe.nbNoeuds(); i++) calculerOrdreParcours(graphe, i);
 
-        fill(estVu.begin(), estVu.end(), false);
+        fill(m_estVu.begin(), m_estVu.end(), false);
         const Graphe grapheInverse = inverserArcs(graphe);
 
-        for (int i=aVisiter.size()-1; i>=0; i--) {
-            const int noeud = aVisiter[i];
-            if (!estVu[noeud]) {
-                ajouterDansCFC(grapheInverse, noeud, nbCFCs);
-                nbCFCs++;
+        for (int i=m_aVisiter.size()-1; i>=0; i--) {
+            const int noeud = m_aVisiter[i];
+            if (!m_estVu[noeud]) {
+                ajouterDansCFC(grapheInverse, noeud, m_nbCFCs);
+                m_nbCFCs++;
             }
         }
     }
 
-    int nbCFCs;
-    vector<int> CFCs;
+    int nbCFCs () const
+    {
+        return m_nbCFCs;
+    }
+
+    int CFC (const int noeud) const
+    {
+        return m_CFCs[noeud];
+    }
+
 
 private:
     void calculerOrdreParcours (const Graphe &g, const int noeud)
     {
-        if (estVu[noeud]) return;
-        estVu[noeud] = true;
+        if (m_estVu[noeud]) return;
+        m_estVu[noeud] = true;
 
         for (int i=0; i<g.nbVoisins(noeud); i++) {
             calculerOrdreParcours(g, g.voisin(noeud, i));
         }
 
-        aVisiter.push_back(noeud);
+        m_aVisiter.push_back(noeud);
     }
 
     void ajouterDansCFC (const Graphe &g, const int noeud, const int idCFC)
     {
-        if (estVu[noeud]) return;
-        estVu[noeud] = true;
+        if (m_estVu[noeud]) return;
+        m_estVu[noeud] = true;
 
-        CFCs[noeud] = idCFC;
+        m_CFCs[noeud] = idCFC;
 
         for (int i=0; i<g.nbVoisins(noeud); i++) {
             ajouterDansCFC(g, g.voisin(noeud, i), idCFC);
         }
     }
 
-    vector<int> aVisiter;
-    vector<bool> estVu;
+    vector<int> m_aVisiter;
+    vector<bool> m_estVu;
+
+    int m_nbCFCs;
+    vector<int> m_CFCs;
 };
 
 #endif // KOSARAJU_HPP
