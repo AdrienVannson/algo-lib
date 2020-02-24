6c782a5d7b14ccf0fb1fd2b3a2ac8cea13f69c75
Adrien VANNSON <adrien.vannson@protonmail.com>
2020-02-24 15:20:22 +0100

    Create Graph

---
+++ include/graphs/data-structures/Graph.hpp
@@ -0,0 +1,68 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include "global.hpp"
+
+
+class Graph
+{
+public:
+
+    Graph (const int verticeCount, const bool isDirected) :
+        m_isDirected (isDirected),
+        m_edgeCount(0)
+    {
+        m_vertice.resize(verticeCount);
+    }
+
+    inline int verticeCount () const
+    {
+        return m_vertice.size();
+    }
+
+    inline int neighbourCount (const int vertex) const
+    {
+        return m_vertice[vertex].size();
+    }
+
+    /// \brief Returns the id of the n-th neighbour of a vertex
+    inline int neighbour (const int vertex, const int neighbourPos) const
+    {
+        return m_vertice[vertex][neighbourPos].first;
+    }
+
+    /// \brief Returns the id of the n-th out-edge of a vertex
+    inline int edgeId (const int vertex, const int neighbourPos) const
+    {
+        return m_vertice[vertex][neighbourPos].second;
+    }
+
+    /// \brief Returns the id of the first edge from one vertex to another (-1 if it doesn't exist)
+    inline int edgeIdTo (const int vertex, const int neighbour)
+    {
+        for (int i=0; i<neighbourCount(vertex); i++) {
+            if (m_vertice[vertex][i].first == neighbour) {
+                return m_vertice[vertex][i].second;
+            }
+        }
+        return -1;
+    }
+
+    /// \brief Creates a new edge between two vertice
+    inline void addEdge (const int vertex1, const int vertex2)
+    {
+        m_vertice[vertex1].push_back(make_pair(vertex2, m_edgeCount));
+        if (!m_isDirected) {
+            m_vertice[vertex2].push_back(make_pair(vertex1, m_edgeCount));
+        }
+
+        m_edgeCount++;
+    }
+
+private:
+    bool m_isDirected;
+    int m_edgeCount;
+    vector<vector<pair<int, int>>> m_vertice; // [vertex][neighbourPos] --> {neighbourID, edgeID}
+};
+
+#endif // GRAPH_HPP
