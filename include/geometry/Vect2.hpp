dbbd7aaa3bcee1025f21c1d6bb9064da47de4b81
Adrien Vannson <adrien.vannson@protonmail.com>
2021-01-05 19:52:09 +0100

    Update Vect2

--- include/geometry/Vect2.hpp
+++ include/geometry/Vect2.hpp
@@ -1,14 +1,14 @@
-#ifndef VECT_HPP
-#define VECT_HPP
+#ifndef VECT2_HPP
+#define VECT2_HPP
 
 #include "global.hpp"
 
 
 template<class T>
-class Vect
+class Vect2
 {
 public:
-    inline explicit Vect (const T x_=0, const T y_=0) :
+    inline explicit Vect2 (const T x_=0, const T y_=0) :
         x (x_),
         y (y_)
     {}
@@ -31,57 +31,57 @@ public:
         y /= currentNorm;
     }
 
-    inline Vect<T> operator- () const { return Vect<T> (-x, -y); };
+    inline Vect2<T> operator- () const { return Vect2<T> (-x, -y); };
 
     inline void operator*= (const T k) { x *= k; y *= k; }
     inline void operator/= (const T k) { x /= k; y /= k; }
-    inline void operator+= (const Vect<T> &v) { x += v.x; y += v.y; }
-    inline void operator-= (const Vect<T> &v) { x -= v.x; y -= v.y; }
+    inline void operator+= (const Vect2<T> &v) { x += v.x; y += v.y; }
+    inline void operator-= (const Vect2<T> &v) { x -= v.x; y -= v.y; }
 
     T x, y;
 };
 
 template<class T>
-inline Vect<T> operator+ (const Vect<T> &a, const Vect<T> &b)
+inline Vect2<T> operator+ (const Vect2<T> &a, const Vect2<T> &b)
 {
-    return Vect<T>(a.x+b.x, a.y+b.y);
+    return Vect2<T>(a.x+b.x, a.y+b.y);
 }
 
 template<class T>
-inline Vect<T> operator- (const Vect<T> &a, const Vect<T> &b)
+inline Vect2<T> operator- (const Vect2<T> &a, const Vect2<T> &b)
 {
-    return Vect<T>(a.x-b.x, a.y-b.y);
+    return Vect2<T>(a.x-b.x, a.y-b.y);
 }
 
 template<class T>
-inline T operator* (const Vect<T> &a, const Vect<T> &b) // Dot product
+inline T operator* (const Vect2<T> &a, const Vect2<T> &b) // Dot product
 {
     return a.x*b.x + a.y*b.y;
 }
 
 template<class T>
-inline T operator^ (const Vect<T> &a, const Vect<T> &b) // Cross product on z
+inline T operator^ (const Vect2<T> &a, const Vect2<T> &b) // Cross product on z
 {
     return a.x * b.y - b.x * a.y;
 }
 
 template<class T>
-inline Vect<T> operator* (const T k, const Vect<T> &v)
+inline Vect2<T> operator* (const T k, const Vect2<T> &v)
 {
-    return Vect<T>(k*v.x, k*v.y);
+    return Vect2<T>(k*v.x, k*v.y);
 }
 
 template<class T>
-inline Vect<T> operator* (const Vect<T> &v, const T k)
+inline Vect2<T> operator* (const Vect2<T> &v, const T k)
 {
-    return Vect<T>(k*v.x, k*v.y);
+    return Vect2<T>(k*v.x, k*v.y);
 }
 
 template<class T>
-inline double getDist (const Vect<T> &a, const Vect<T> &b)
+inline double getDist (const Vect2<T> &a, const Vect2<T> &b)
 {
     return (a-b).norm();
 }
 
 
-#endif // VECT_HPP
+#endif // VECT2_HPP
