d6f4575f49b2a40d877aaf27cb743a73e2c73139
Adrien Vannson <adrien.vannson@protonmail.com>
2021-11-13 18:51:34 +0100

    Update algolib

--- include/algolib.hpp
+++ include/algolib.hpp
@@ -31,8 +31,9 @@
 #include "sorting/merge-sort.hpp" // ONLY_IF mergeSort
 
 #include "2SAT.hpp" // ONLY_IF Resoudre2SAT
+#include "strings/automaton.hpp" // ONLY_IF Automaton Aut
 #include "strings/kmp.hpp" // ONLY_IF getKmp
-#include "strings/regex.hpp" // ONLY_IF Regex
+#include "strings/regex.hpp" // ONLY_IF Regex Reg
 
 #include "constants.hpp" // ONLY_IF Constants
 #include "json.hpp" // ONLY_IF JSON
