ff560a9d9df4a30544647f00491773312845e296
Adrien Vannson <adrien.vannson@protonmail.com>
2021-12-24 00:34:50 +0100

    Add verticeCount to Tree

--- include/trees/Tree.hpp
+++ include/trees/Tree.hpp
@@ -10,6 +10,11 @@ public:
     Tree();
     Tree(const int verticeCount, const int root = 0);
 
+    inline int verticeCount() const
+    {
+        return m_vertice.size();
+    }
+
     inline int root() const
     {
         return m_root;
