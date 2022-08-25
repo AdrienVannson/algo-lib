44020c797305188220746209a1f5a1f6c0d3ef52
Adrien Vannson <adrien.vannson@protonmail.com>
2022-08-25 12:03:09 +0200

    Rewrite merge-files.py

--- tools/merge-files.py
+++ tools/merge-files.py
@@ -1,124 +1,71 @@
 import re, os.path
 
-dossiersInclusion = [ os.getcwd() ]
+def lines_of_file(fn):
+    try:
+        with open(fn, 'r') as f:
+            return [l[:-1] for l in f.readlines()] # Remove \n at the end of each line
+    except:
+        return []
+
+# Folders where files to include can be located
+path_folders = [os.getcwd(), '']
 for i in range(int(input())):
-    dossiersInclusion.append(os.path.abspath(input()))
+    path_folders.append(os.path.abspath(input()))
 
-fichiersSources = []
-for i in range(int(input())):
-    fichiersSources.append(os.path.abspath(input()))
+# Contains the remaining lines to parse. The last line is the next one to be parsed
+pending = list(reversed(lines_of_file('main.cpp')))
 
+output = []
+included_files = []
 
-isPreviousLineEmpty = True # Remove consecutive empty lines
+while len(pending):
+    line = pending[-1]
+    pending.pop()
 
-includedFiles = []
-words = set() # All the words contained in the files
+    if re.match("^#include \"*\"", line):
+        if ' // ONLY_IF ' in line:
+            line = line.split(' // ONLY_IF ')[0]
 
-firstLines = set() # Lines that should be added only once, at the beginning of the file
-outputLines = []
+        relative_path = re.sub("^\\#include \"(.+)\"$", "\\1", line)
 
-def include(path, isRecursive):
-    global isPreviousLineEmpty
+        file_exists = False
 
-    path = os.path.realpath(path)
+        for include_folder in path_folders:
+            folder = os.path.realpath(include_folder + '/' + os.path.dirname(relative_path))
+            file = folder + '/' + os.path.basename(relative_path)
 
-    if path in includedFiles:
-        return
-    includedFiles.append(path)
+            if os.path.exists(file):
+                # Check if the file has already been included
+                if file in included_files:
+                    file_exists = True
+                    break
+                included_files.append(file)
 
-    content = open(path, 'r').read().split('\n')
+                # Add the folder to the path to find the next included files
+                if not folder in path_folders:
+                    path_folders.append(folder)
 
-    # Words contained in the file
-    currentWord = []
-    for l in content:
-        if re.match("^#include ", l):
-            continue
-        for c in l + ' ':
-            if c.isalpha() or c.isdigit() or c == '_':
-                currentWord.append(c)
-            else:
-                words.add(''.join(currentWord))
-                currentWord = []
+                # Add the .cpp if needed
+                if file[-4:] == '.hpp':
+                    source_file = file[:-4] + '.cpp'
+                    if os.path.exists(source_file):
+                        pending.append('#include "' + source_file + '"')
 
-    for line in content:
-        # The line should appear only once, at the beginning of the file
-        if re.match("^#include <", line) or line == "using namespace std;":
-            firstLines.add(line)
+                # Add the content of the file
+                for l in reversed(lines_of_file(file)):
+                    pending.append(l)
 
-        # Include
-        elif re.match("^#include \"*\"", line):
-            if ' // ONLY_IF ' in line:
-                line, motsRequis = line.split(' // ONLY_IF ')
-                motsRequis = motsRequis.split()
+                file_exists = True
+                break
 
-                success = False
-                for w in motsRequis:
-                    if w in words:
-                        success = True
-                if not success:
-                    continue
+        if not file_exists:
+            print("Can't find file", relative_path)
+            print(line)
 
-            if isRecursive:
-                nouveauFichier = re.sub("^\\#include \"(.+)\"$", "\\1", line)
-
-                succes = False
-
-                for dossierInclusion in dossiersInclusion:
-                    hauteur = 0
-
-                    while True:
-                        fichierSeul = os.path.basename(nouveauFichier)
-                        dossier = os.path.realpath(dossierInclusion + '/' + (hauteur * '../') + os.path.dirname(nouveauFichier))
-                        fichier = dossier + '/' + fichierSeul
-
-                        if os.path.exists(fichier):
-                            if not dossier in dossiersInclusion:
-                                dossiersInclusion.append(dossier)
-
-                            include(fichier, True)
-                            succes = True
-                            break
-
-                        if os.path.realpath(dossierInclusion + '/' + (hauteur * '../')) == '/':
-                            break
-
-                        hauteur += 1
-
-                if not succes:
-                    print("ERREUR", nouveauFichier)
-
-        # Empty line
-        elif line == '':
-            if not isPreviousLineEmpty:
-                outputLines.append('')
-                isPreviousLineEmpty = True
-
-        # The header guards are not written in the output file
-        elif not re.match("^#ifndef .*_HPP$", line) \
-         and not re.match("^#define .*_HPP$", line) \
-         and not re.match("^#endif .*_HPP", line):
-               outputLines.append(line)
-               isPreviousLineEmpty = False
-
-
-include('main.cpp', True)
-
-# Add the .cpp files corresponding to the .hpp files
-for header in includedFiles:
-    source = re.sub("^(.+)\\.h(pp)?$", "\\1.cpp", header)
-
-    if not source == 'main.cpp' and os.path.exists(source):
-        include(source, False)
-
-for fichierSource in fichiersSources:
-    include(fichierSource, False)
+    else:
+        output.append(line)
 
 # Write output.cpp
-output = open('output.cpp', 'w')
-
-for l in sorted(list(firstLines)):
-    output.write(l + '\n')
-output.write('\n')
-
-for l in outputLines:
-    output.write(l + '\n')
+with open('output.cpp', 'w') as output_file:
+    for l in output:
+        output_file.write(l + '\n')
