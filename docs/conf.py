fd98c41b751f749a6fb6e7c59239726cd7c31c3a
Adrien Vannson <adrien.vannson@protonmail.com>
2023-02-16 20:44:28 +0100

    Add project name to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -7,3 +7,8 @@ html_theme = "sphinx_rtd_theme"
 # Breathe configuration
 breathe_default_project = "algolib"
 breathe_default_members = ('members', 'undoc-members')
+
+# Project informations
+project = 'Algolib'
+copyright = 'Adrien Vannson'
+author = 'Adrien Vannson'
