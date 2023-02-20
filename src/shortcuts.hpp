33905b89ab7494f87eecb2c36acfeed2d9d18b3c
Adrien Vannson <adrien.vannson@protonmail.com>
2023-02-20 19:10:44 +0100

    Add a file containing shortcuts

---
+++ src/shortcuts.hpp
@@ -0,0 +1,16 @@
+#ifndef SHORTCUTS_HPP
+#define SHORTCUTS_HPP
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+using Vi = vector<int>;
+using Vii = vector<vector<int>>;
+
+#define for_n(i, n) for (int i = 0; i < (n); i++)
+#define for_range(i, a, b) for (int i = (a); i < (b); i++)
+
+#endif // SHORTCUTS_HPP
