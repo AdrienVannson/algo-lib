68c7a268337cd4fdabeffba432f4ba3197a98a6e
Adrien Vannson <adrien.vannson@protonmail.com>
2025-03-09 15:01:02 +0100

    Force convertion in for_n loops

--- src/shortcuts.hpp
+++ src/shortcuts.hpp
@@ -9,5 +9,5 @@ using ull = unsigned long long;
 using Vi = vector<int>;
 using Vii = vector<vector<int>>;
 
-#define for_n(i, n) for (int i = 0; i < (n); i++)
-#define for_range(i, a, b) for (int i = (a); i < (b); i++)
+#define for_n(i, n) for (int i = 0; i < int(n); i++)
+#define for_range(i, a, b) for (int i = (a); i < int(b); i++)
